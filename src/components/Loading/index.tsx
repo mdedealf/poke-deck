653ff181f837954aec87875e6475e4095724a41f
mdedealf <dede.alfaruqq@gmail.com>
2024-10-06 22:31:31 +0700

    chore: edit loading heigh screen

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Loading: FC = () => {
   return (
-    <div className="h-[calc(100vh-98px)] min-w-[430px] max-w-[430px] bg-[#252a3e] text-white font-bold text-[24px] flex items-center justify-center">
+    <div className="h-screen min-w-[430px] max-w-[430px] bg-[#252a3e] text-white font-bold text-[24px] flex items-center justify-center">
       Loading...
     </div>
   );
