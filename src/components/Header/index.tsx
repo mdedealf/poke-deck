3396d5cef5123e38757f77196a7b89b46650437d
mdedealf <dede.alfaruqq@gmail.com>
2024-10-06 19:13:34 +0700

    feat: adding custom debounce for searching

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,21 +6,31 @@ import { Link, useNavigate } from "react-router-dom";
 const Header: FC = () => {
   const [isSearch, setIsSearch] = useState(false);
   const [input, setInput] = useState<string>("");
+  const [isSearching, setIsSearching] = useState<boolean>(false);
   const navigate = useNavigate();
 
+  // Handle when user input change
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setInput(e.target.value);
   };
 
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>): void => {
-    if (e.key === "Enter") {
+  // Handle when user press enter in the keyboard
+  const handleSearchPokemon = (e: KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === "Enter" && !isSearching) {
+      setIsSearching(true);
+
       const inputLowerCase = input.toLowerCase();
       navigate(`/details/${inputLowerCase}`);
+
+      // Custom debounce 4s
+      setTimeout(() => {
+        setIsSearching(false);
+      }, 4000);
     }
   };
 
   // Handle when user click search icon
-  const handleSearchInput = () => {
+  const handleSearchIcon = () => {
     setIsSearch((prev) => !prev);
   };
 
@@ -37,11 +47,12 @@ const Header: FC = () => {
             placeholder="Search..."
             value={input}
             onChange={handleInputChange}
-            onKeyDown={handleKeyDown}
+            onKeyDown={handleSearchPokemon}
+            disabled={isSearching}
           />
           <span
             className="flex items-center justify-center cursor-pointer font-bold leading-[0px] text-[16px] bg-white text-black h-[22px] w-[22px] rounded-2xl hover:bg-slate-200 p-0 m-0 transition-all"
-            onClick={handleSearchInput}
+            onClick={handleSearchIcon}
           >
             x
           </span>
@@ -50,7 +61,7 @@ const Header: FC = () => {
         <>
           <img
             className="cursor-pointer"
-            onClick={handleSearchInput}
+            onClick={handleSearchIcon}
             src={SearchIcon}
             alt="Search icon"
           />
